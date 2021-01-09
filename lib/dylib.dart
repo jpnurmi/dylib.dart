c75312f331ad34189f36c590e995c2afc2434d7b
J-P Nurmi <jpnurmi@gmail.com>
2021-01-09 17:34:24 +0100

    Import Dart package template

---
+++ lib/dylib.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dylib;
+
+export 'src/dylib_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
